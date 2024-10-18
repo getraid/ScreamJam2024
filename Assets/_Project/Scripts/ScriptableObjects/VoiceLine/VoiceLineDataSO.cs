2598d6c06c4751bc96363a62d54f161da100527b
Lukáš Hojdysz <lukashojdysz@hotmail.com>
2024-10-18 21:01:07 +0200

    VoiceLinesSubtitleManager
    +Singleton in the scene
    +Subtitles and voicelines in SO, synchronization is based on audio length so audio needs to be always present for now

---
+++ Assets/_Project/Scripts/ScriptableObjects/VoiceLine/VoiceLineDataSO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/VoiceLineData", order = 1)]
+public class VoiceLineDataSO : ScriptableObject
+{
+    [field:SerializeField] public List<VoiceLineData> VoiceLineDataVoiceLines { get; set; }
+
+}
+
+[Serializable]
+public class VoiceLineData
+{
+    [TextArea(2, 5)] 
+    public string Text;
+    public AudioClip Audio;
+    public float TimeDelay;
+}
+
+
