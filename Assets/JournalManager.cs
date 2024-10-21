544f5cf7dfa04e21cd8728e35882c5ab3531d866
getraid <32432718+getraid@users.noreply.github.com>
2024-10-21 21:31:05 +0200

    Update Journalmgr + Readded monster

--- Assets/JournalManager.cs
+++ Assets/JournalManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class JournalManager : MonoBehaviour
 {
+    
+    [field: SerializeField] public Animator Animator { get; set; }
+    
+    
+    private static readonly int HasOpened = Animator.StringToHash("HasOpened");
+    private static readonly int IsReady = Animator.StringToHash("IsReady");
+
+    private bool testFlipFlop = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,13 @@ public class JournalManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            testFlipFlop = !testFlipFlop;
+            Animator.SetBool(HasOpened, testFlipFlop);
+            Animator.SetBool(IsReady,true);
+        }
+        
         
     }
 }
